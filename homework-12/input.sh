8c46948c581160e5b122a023b0d8c673931c9c5a
EC2 Default User <ec2-user@ip-172-31-83-232.ec2.internal>
2025-06-09 20:43:51 +0000

    homework-12

---
+++ homework-12/input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+read -p what is your name?: Meerim 
+read -p what is your age: 33
+read -p what is your favorite_language: bash 
+read -p what is your hobby: walking 
+read -p what is your dream: become devop engineer 
+
+echo "Hi, my name is Meerim, and I'm 33 years old. Every morning, Meerim likes to spend time walking. As a 33 years old, I believe it's never too late to follow your dreams. My favoritre programming language is bash. Icode in bash almost everyday! One day I hope my love for bash will help me achieve my dream of becoming a devop engineer."When
+I'm not busy working toward becoming devop engineer, you will find me walking to relax." That's a little about me, Meerim a 33 year old who loves bash, enjoys walking, and dreams of becoming a devop." 
+
+
---
+++ homework-12/introduce.sh
@@ -0,0 +1,10 @@
+#!bin/bash 
+
+name=Meerim
+age=33
+favorite_language=bash
+hobby=walking
+dream=to become a devop engineer 
+
+echo "Hi, my name is Meerim, and I'm 33 years old. Every morning, Meerim likes to spend time walking. As a 33 years old, I believe it's never too late to follow your dreams. My favoritre programming language is bash. I code in bash almost everyday! One day I hope  my love for bash  will help me achieve my dream of becoming a devop engineer" When I'm not busy working toward becoming devop engineer, you will find me walking to relax." That's a little about me, Meerim a 33 year old who loves bash, enjoys walking, and dreams of becoming a devop engineer!" 
+
